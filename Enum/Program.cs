652da09bcf07a3cde4ac81ef57c05b1ba722292a
mdsakhawatullah <75324807+mdsakhawatullah@users.noreply.github.com>
2024-06-24 21:24:35 +0600

    file added

---
+++ Enum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+enum Months
+{
+    January,
+    February,
+    March,
+    April,
+    May,
+    June,
+    July,
+    August,
+    September,
+    Octobor,
+    November,
+    December
+}
+
+class Program
+{
+    public static void Main()
+    {
+        Months[] months = (Months[])Enum.GetValues(typeof(Months));
+        foreach (var month in months)
+        {
+            
+                Console.WriteLine($"Month name: {month}");
+            
+        }
+    }
+
+}
